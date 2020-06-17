1ed6a0ff728f3a1967c8c017d0b7321d9381ee11
laozhou1204 <zhouqi1204@gmail.com>
2020-06-17 17:21:28 +0800

    222

--- C189Checkin.py
+++ C189Checkin.py
@@ -7,8 +7,8 @@ username = ""
 password = ""
 
 if(username == "" or password == ""):
-    username = input("账号：")
-    password = input("密码：")
+    username = input("账号：13229222311")
+    password = input("密码：zhou841224")
 
 def main():
     login(username, password)
